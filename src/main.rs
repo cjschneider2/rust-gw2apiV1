4c58826eb47609534aa873b9ff92bce5542ac73f
cjschneider2 <cjschneider2@gmail.com>
2014-07-02 22:28:55 +0200

    added Ingredient struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 extern crate http;
 extern crate serialize;
 
-//use std::os;
-//use std::str;
-//use std::io::println;
-//use http::headers::HeaderEnum;
 use serialize::json;
 use serialize::{Decoder, Decodable};
 use http::client::RequestWriter;
@@ -18,21 +14,42 @@ pub struct ApiBuild{
 pub struct ApiItems{
     items: Vec<int>,
 }
+#[deriving(Decodable)]
+pub struct ApiRecipeDetails{
+    recipe_id: int,
+    type_: String,
+    output_item_id: String,
+    output_item_count: String,
+    min_rating: String,
+    time_to_craft_ms: String,
+    vendor_value: String,
+    disciplines: Vec<String>,
+    flags: Vec<String>,
+    ingredients: Vec<ApiIngredient>,
+}
+#[deriving(Decodable)]
+pub struct ApiIngredient{
+    item_id: String,
+    count: String,
+}
 
 fn main() {
-    format!("{}", Get);
     // Build ID
-    /*
     let api_build_url = "https://api.guildwars2.com/v1/build.json";
     let api_build_data = make_api_request(api_build_url.as_slice());
     let GW2apiBuild: ApiBuild = decode_api_data(api_build_data);
     println!("Build ID: {}",GW2apiBuild.build_id);
+    /*
     // Items
-    */
     let api_items_url = "https://api.guildwars2.com/v1/items.json";
     let api_items_data = make_api_request(api_items_url.as_slice());
     let GW2apiItems: ApiItems = decode_api_data(api_items_data);
     println!("Build ID: {}",GW2apiItems.items);
+    */
+    let api_RecipeDetails_url = "https://api.guildwars2.com/v1/recipe_details.json?recipe_id=1275";
+    let api_RecipeDetails_data = make_api_request(api_RecipeDetails_url.as_slice());
+    let GW2apiRecipeDetails: ApiRecipeDetails = decode_api_data(api_RecipeDetails_data);
+    println!("{}",GW2apiRecipeDetails.type_);
 }
 
 fn decode_api_data<T: Decodable<json::Decoder,json::DecoderError>>(strData: Box<String>) -> T{
